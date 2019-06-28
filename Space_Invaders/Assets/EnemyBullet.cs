68dba464e25e55b557a1d6bef3d9dacb64299f4d
Omarree <Omarree.Kimbrough@digipen.edu>
2019-06-28 16:26:19 -0700

    FIXED!!!!!!!!

--- Space_Invaders/Assets/EnemyBullet.cs
+++ Space_Invaders/Assets/EnemyBullet.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
-    public Gameobject bullet;
-    public KeyCode fire = KeyCode.space;
+    public GameObject bullet;
+    public KeyCode fire = KeyCode.Space;
 
     // Start is called before the first frame update
     void Start()
