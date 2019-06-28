b9176ff31b01d9c9ed6db5024c9762d2821848ad
Siddharth <s.ghali@digipen.edu>
2019-06-28 15:51:06 -0700

    shooting mechanics

---
+++ Space_Invaders/Assets/ShootingMechanic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingMechanic : MonoBehaviour
+{
+
+    public GameObject bullet;
+
+    public KeyCode fire = KeyCode.Space;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(fire))
+        {
+            GameObject newBullet = Instantiate
+
+        }
+        
+    }
+}
