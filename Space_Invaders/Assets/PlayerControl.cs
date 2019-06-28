027bb79ed3a47f4a26c93e72d764f528c6c2be43
Siddharth <s.ghali@digipen.edu>
2019-06-28 11:32:57 -0700

    Pre lunch commit

---
+++ Space_Invaders/Assets/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour
+{
+    Rigidbody rb;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = Vector3.zero;
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+           direction.x = speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x = -speed;
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+        rb.velocity = direction;
+    }
+}
