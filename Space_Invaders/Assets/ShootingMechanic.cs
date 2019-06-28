9d0ada3156880766d83f6915363e620636f64b7f
Omarree <Omarree.Kimbrough@digipen.edu>
2019-06-28 16:07:28 -0700

    [p

--- Space_Invaders/Assets/ShootingMechanic.cs
+++ Space_Invaders/Assets/ShootingMechanic.cs
@@ -5,24 +5,6 @@ using UnityEngine;
 public class ShootingMechanic : MonoBehaviour
 {
 
-    public GameObject bullet;
-
-    public KeyCode fire = KeyCode.Space;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(fire))
-        {
-            GameObject newBullet = Instantiate
-
-        }
-        
-    }
 }
+
+   
