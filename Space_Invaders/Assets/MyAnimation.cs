539dca1f520cbf6a8dc2099f0c84c23a68793b00
Omarree <Omarree.Kimbrough@digipen.edu>
2019-06-28 11:30:16 -0700

    adsa

---
+++ Space_Invaders/Assets/MyAnimation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyAnimation : MonoBehaviour
+{
+    public Sprite[] sprites;
+    int currentsprite = 0;
+    public float FramesPerSecond;
+    float SecondsPerFrame;
+    float timeUntilChange;
+
+    void UpdateFramesPerSecond()
+    {
+        if (FramesPerSecond == 0)
+            FramesPerSecond = 0.0001f;
+        SecondsPerFrame = 1f / FramesPerSecond;
+    }
+
+
+    void Start()
+    {
+        UpdateFramesPerSecond();
+        timeUntilChange = SecondsPerFrame;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateFramesPerSecond();
+        GetComponent<SpriteRenderer>().sprite = sprites[currentsprite];
+        timeUntilChange -= Time.deltaTime;
+        if (FramesPerSecond > 0 && timeUntilChange < 0)
+        {
+            timeUntilChange = SecondsPerFrame;
+            currentsprite++;
+            if (currentsprite >= sprites.Length)
+                currentsprite = 0;
+        }
+        else if (FramesPerSecond < 0 && timeUntilChange < 0)
+        {
+            timeUntilChange = -SecondsPerFrame;
+            currentsprite--;
+            if (currentsprite < 0)
+                currentsprite = sprites.Length - 1;
+        }
+       
+    }
+}
