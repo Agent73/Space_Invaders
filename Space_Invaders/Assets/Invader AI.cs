73b26f6117fe2eb7c38fe6480e569c8b610b40bc
Omarree <Omarree.Kimbrough@digipen.edu>
2019-06-28 13:17:26 -0700

    AI

--- Space_Invaders/Assets/Invader AI.cs
+++ Space_Invaders/Assets/Invader AI.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class InvaderAI : MonoBehaviour
 {
+    Rigidbody rb;
+    float speed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector3(speed, rb.velocity.y, 0);
+    }
+    private void OnDrawGizmos()
+    {
+        if(rb)
+            Gizmos.DrawLine(transform.position, transform.position + rb.velocity); 
     }
 }
