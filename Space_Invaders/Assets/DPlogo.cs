d207fc0bb84c7cd6c6f8d11bd3649aa6f35b25d5
Omarree <Omarree.Kimbrough@digipen.edu>
2019-07-03 13:46:06 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Space_Invaders/Assets/DPlogo.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DPlogo : MonoBehaviour
+{
+    public const float TIME_LIMIT = 3F;
+    private float timer = 0F;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.timer += Time.deltaTime;
+        if (this.timer >= TIME_LIMIT)
+        {
+            SceneManager.LoadScene("Logo SplashScreen");
+        }
+    }
+
+}
